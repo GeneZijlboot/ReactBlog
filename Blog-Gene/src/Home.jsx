14c8d7e964a456e2249bdcbe0300e7c575bdabbf
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-09-29 15:00:52 +0200

    making a custome hook for fetching data

--- Blog-Gene/src/Home.jsx
+++ Blog-Gene/src/Home.jsx
@@ -1,26 +1,11 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList';
  
 function Home(){
-    const [blogs, setBlogs] = useState(null);
-    const [ispending, Setispending] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                Setispending(false);
-            })
-        }, 1000);
-    }, [])
-
+    const { data: blogs, ispending, error} = useFetch('http://localhost:8000/blogs')
     return (  
         <div className="home">
+            {error && <h1>{error}</h1>}
             {ispending && <h1>Loading...</h1>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
---
+++ Blog-Gene/src/useFetch.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [ispending, setIspending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+            .then((res) => {
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setData(data);
+                setIspending(false);
+                setError(null);
+            })
+            .catch((error) => {
+                console.log(error.message);
+                setError(error.message);
+                setIspending(false);
+            });
+        }, 1000);
+    }, [url]);
+
+    return {data, ispending, error}
+}
+
+export default useFetch;
