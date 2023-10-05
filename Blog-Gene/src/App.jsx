c8062be77b21a972e68ccec37396a0f547727d77
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-10-05 14:41:48 +0200

    using react route to make page-swapping faster

--- Blog-Gene/src/App.jsx
+++ Blog-Gene/src/App.jsx
@@ -1,14 +1,25 @@
 import NavBar from './NavBar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
     return (
-        <div className="App">
-            <NavBar />
-            <div className="content">
-                <Home />
-            </div>
-        </div> 
+        <Router>
+            <div className="App">
+                <NavBar />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/create">
+                            <Create />
+                        </Route>
+                    </Switch>
+                </div>
+            </div> 
+        </Router>
     );
 }
 
